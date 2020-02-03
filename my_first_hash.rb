7f6eaf2739e77f017f5772c7669e941dccd0fca4
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 19:58:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,8 +30,9 @@ end
 
 def adding
   shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  musket[]
+  shipping_manifest[musket] = 2 
+  shipping_manifest[gun powder] = 4
   # return the shipping_manifest hash below
   
-  shipping_manifest[oil paintings]
+  shipping_manifest
 end
